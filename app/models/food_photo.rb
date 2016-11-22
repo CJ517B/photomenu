47d0e6d356d984cee492366e2977f1badca0f148
Raghu Betina <raghu@firstdraft.com>
2016-11-22 04:25:53 +0000

    Direct association between users and food_photos

--- app/models/food_photo.rb
+++ app/models/food_photo.rb
@@ -1,6 +1,8 @@
 class FoodPhoto < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :photos,
+             :class_name => "FoodPhoto",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
