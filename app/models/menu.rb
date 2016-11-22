68d984c232c831613a7d2c4a15e4d071a05f01d9
Raghu Betina <raghu@firstdraft.com>
2016-11-22 04:25:53 +0000

    Direct association between menus and menu_listings

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,9 @@
 class Menu < ApplicationRecord
   # Direct associations
 
+  has_many   :menu_listings,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/menu_listing.rb
+++ app/models/menu_listing.rb
@@ -1,6 +1,8 @@
 class MenuListing < ApplicationRecord
   # Direct associations
 
+  belongs_to :menu
+
   belongs_to :restaurant
 
   # Indirect associations
