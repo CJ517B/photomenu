4bbbe009e1015ebb8dd6e45e8768b30164b941b7
Raghu Betina <raghu@firstdraft.com>
2016-11-22 04:26:23 +0000

    Whitelist columns in WantToOrder for ActiveAdmin

--- app/admin/want_to_order.rb
+++ app/admin/want_to_order.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register WantToOrder do
 
+ permit_params :user_id, :menu_item_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
