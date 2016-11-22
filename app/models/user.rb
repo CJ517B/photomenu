b92bd5ed771b143412f455c54045d51b75ab5fae
Raghu Betina <raghu@firstdraft.com>
2016-11-22 04:25:53 +0000

    Direct association between users and want_to_orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :want_to_orders,
+             :dependent => :destroy
+
   has_many   :photos,
              :class_name => "FoodPhoto",
              :dependent => :destroy
--- app/models/want_to_order.rb
+++ app/models/want_to_order.rb
@@ -1,6 +1,8 @@
 class WantToOrder < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
